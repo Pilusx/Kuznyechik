a9da137dde6d0ce85725642682aa6893b722ff44
qa <zbigniew.kaczynski@op.pl>
2022-01-10 11:28:30 +0100

    TransformL

---
+++ transformL.s
@@ -0,0 +1,17 @@
+.include "common.s"
+
+.text
+.globl main
+main:
+    call   read_block # Reads bytes in the big endian order.
+    call   change_endianness # Change to little endian.
+
+    movaps %xmm0, %xmm2
+
+    call   transform_L
+
+    movaps %xmm2, %xmm0
+
+    call   change_endianness # Change to big endian.
+    call   write_block
+    jmp    exit
